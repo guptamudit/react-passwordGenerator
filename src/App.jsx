293220b1c933c25956d4f14e6656294c5f060989
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2023-10-08 09:12:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,22 +29,25 @@ function App() {
   return (
     <>
       <div className="rounded-lg w-full max-w-md mx-auto text-center px-4 my-8 text-2xl text-white flex flex-col bg-gray-500">
-        <p className="mt-[13px]">Password Generator</p>
+        <p className="mt-[13px] pr-[2.25rem] md:pr-0">Password Generator</p>
 
-        <div className="flex flex-row items-center overflow-hidden mt-4 mb-4">
+        <div className="flex flex-wrap flex-col	 items-center overflow-hidden mt-4 mb-4 md:flex-row">
           <input
             type="text"
             value={pass}
-            className="outline-none py-1 px-3 bg-transparent text-white text-[20px] bg-slate-600 rounded-lg ml-[37px]" // Add background and text color styles
+            className="outline-none py-1 px-3 bg-transparent text-white text-[20px] bg-white rounded-lg md:mr-2 md:ml-[32px]"
             placeholder="Password"
             readOnly
           />
           <button
-            className="py-1 px-3 text-[20px] bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 ml-[5px] rounded-lg"
+            className="py-1 px-3 text-[20px] bg-blue-500 text-white rounded-r-lg hover:bg-blue-600  ml-[-177px] mt-[12px] md:ml-[8px] md:mt-0 rounded-lg"
             // onClick={handleButtonClick} // Add your click event handler function
           >
             Copy
           </button>
+        </div>
+        <div className="flex flex-row mb-3">
+          <input className="ml-[83.5px] md:ml-[31px]" type="range" />
         </div>
       </div>
     </>
@@ -52,3 +55,4 @@ function App() {
 }
 
 export default App;
+
